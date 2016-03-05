ba283e6eb80bab883f6c9e71ce493f162e719faa
Justin <LouisLeGros@gmail.com>
2016-03-05 14:59:35 -0800

    Created class for SpaceNeedle, wrote some comments for basic organization

---
+++ 142 Assignments/src/SpaceNeedle.java
@@ -0,0 +1,15 @@
+// This code will print a representation of the space needle scaling on a class constant
+// using ASCII characters. Loops and methods are required to produce the text representation. 
+
+public class SpaceNeedle {
+	
+	public static final int SCALE = 4; // scale for the ASCII
+	
+	public static void main(String[] args) {
+		// tip(SCALE);
+		// topHalf(SCALE);
+		// tip(SCALE)
+		// midSegment(SCALE);
+		// topHalf(SCALE);
+	}
+}
